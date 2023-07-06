d12ccd2519b4b61bb86b98b9f8455b0272599ebf
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-07-06 09:16:42 +0300

    keep_promise

---
+++ 0x00-ES6_basic/tests/0x01-ES6_promise/tests/1-main.js
@@ -0,0 +1,4 @@
+import getFullResponseFromAPI from './1-promise'; // we import from js parkeg
+
+console.log(getFullResponseFromAPI(true)); // we call from import 
+console.log(getFullResponseFromAPI(false));
