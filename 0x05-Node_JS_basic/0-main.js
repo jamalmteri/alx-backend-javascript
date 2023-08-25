209a1d3b34a4d11ab0ccdf7340aeac45bbc38c47
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-08-25 21:02:04 +0300

    node_js_basic

---
+++ 0x05-Node_JS_basic/0-main.js
@@ -0,0 +1,5 @@
+const displayMessage = require('./0-console');
+
+displayMessage("Hello NodeJS!");
+
+//here is ana example but you can create your own code 
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
+
+//when you see any thing stand like arow operator means its a function tha return something
