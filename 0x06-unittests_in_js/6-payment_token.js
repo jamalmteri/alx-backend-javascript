d7c12b4e0fd9da638c300734c4b15f15863da6bc
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-09-06 11:55:51 +0300

    Update 6-payment_token.js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,9 +1,6 @@
-function getPaymentTokenFromAPI (success) {
-    return new Promise(function (resolve) {
-      if (success) {
-        resolve({ data: 'Successful response from the API' });
-      }
-    });
+function getPaymentTokenFromAPI(success) {
+  if (success)
+    return Promise.resolve({ data: 'Successful response from the API' });
 }
 
 module.exports = getPaymentTokenFromAPI;
