61117362328646f8502ec4161f1bb014581bd77a
jamalmteri <110991659+jamalmteri@users.noreply.github.com>
2023-09-06 11:54:17 +0300

    Update 4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,19 +1,24 @@
 const { expect } = require('chai');
-const { it, describe } = require('mocha');
 const sinon = require('sinon');
 
-const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./4-payment.js');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
 
-describe('', () => {
-  const checkSoy = sinon.spy(console, 'log');
-  it('checking if numbers round with spies and stubs', () => {
+describe('sendPaymentRequestToApi', () => {
+  const consoleSpy = sinon.spy(console, 'log');
+  it('validates usage of Utils.calculateNumber', () => {
+    // Stub the function always return the same number 10
+    const calcNumStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    // run the function with the parameters
     sendPaymentRequestToApi(100, 20);
-    const stubBoy = sinon.stub(Utils, 'calculateNumber');
-    stubBoy.withArgs('SUM', 100, 20).returns(120);
-    expect(checkSoy.calledOnce).to.be.true;
-    expect(console.log('The total is: 120')).to.be.all;
-    checkSoy.restore();
-    stubBoy.restore();
+    // check that the stub is being called with type = SUM, a = 100, and b = 20
+    expect(calcNumStub.calledWith('SUM', 100, 20)).to.be.true;
+    // check that the stub always return the same number 10
+    expect(calcNumStub.alwaysReturned(10)).to.be.true;
+    // check with spy that console.log is logging the correct message 
+    expect(consoleSpy.calledWith('The total is: 10')).to.be.true;
+
+    calcNumStub.restore();
+    consoleSpy.restore();
   });
 });
